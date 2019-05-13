1859c2be7a5bb63c496d8f1996c075f062ecedba
craig walker <craigwalker1123@gmail.com>
2019-05-12 22:00:39 -0600

    html dimension template

--- gatsby-ssr.js
+++
@@ -1,4 +0,0 @@
-// import onRenderBodyAnalytics from './src/state/onRenderBodyAnalytics';
-//
-// // adds scripts to the body and head (google analytics)
-// export const onRenderBody = onRenderBodyAnalytics;
